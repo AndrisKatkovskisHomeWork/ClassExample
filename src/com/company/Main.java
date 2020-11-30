f2ff05c6306fd69ccc189a296cd43a27c0e7ac11
crocy@inbox.lv <Ciklops8>
2020-11-30 22:25:00 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class Person {
+
+    String name;
+    int age;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        
+    }
+
+
+}
+
